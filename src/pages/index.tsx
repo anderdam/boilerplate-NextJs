8ce0f7ddd7ee8a742df9008c902ae59dba5dfb89
AndersonD <anderdam@gmail.com>
2021-08-20 14:25:41 -0300

    Add husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,12 @@ import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
 type Props = {
-  title: string,
+  title: string
 }
 
-export default function Home( {title = 'React Avançado'} : Props ) {
+const bla = 'var'
+
+export default function Home({ title = 'React Avançado' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
